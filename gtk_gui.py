fcf6556f3d1253b82e8e1e964ac686103a611912
Uriegas <1930526@upv.edu.mx>
2021-02-03 13:18:36 -0600

    Array of buttons

--- gtk_gui.py
+++ gtk_gui.py
@@ -15,13 +15,17 @@ class MyWindow(Gtk.Window):
         #Buttons
         #Array of buttons
         self.arraybuttons = []
+        self.arraylabels = []
         for i in range(10):
             self.arraybuttons.append( Gtk.Button(label = "Button {}".format(i)) )
+            self.arraylabels.append( Gtk.Label("This is button: {}".format(i)) )
 
         for j in range( len(self.arraybuttons) ):
+            self.arraybuttons[j].add(self.arraylabels[j])
             self.grid.add(self.arraybuttons[j])
 
         self.box.pack_start(self.grid, True, True, 0)
+        
 
         #Bacon button
         self.baconbutton = Gtk.Button(label = "Press me!!")
@@ -32,32 +36,7 @@ class MyWindow(Gtk.Window):
         self.Tunabutton = Gtk.Button(label = "Dont Press Me!!!")
         self.Tunabutton.connect("clicked", self.on_button_clicked)
         self.box.pack_start(self.Tunabutton, True, True, 0)
-        
-        '''
-        self.set_border_width(10)
-        self.set_default_size(400, 200)
-        textbox = Gtk.Box(spacing = 10)
-        label = Gtk.Label(label = "This is my project")
-        hbox = Gtk.Box(spacing=10)
-        hbox.set_homogeneous(False)
-        vbox_left = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        vbox_left.set_homogeneous(False)
-        vbox_right = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        vbox_right.set_homogeneous(False)
 
-        hbox.pack_start(vbox_left, True, True, 0)
-        hbox.pack_start(vbox_right, True, True, 0)
-
-        label = Gtk.Label(label="This is a normal label")
-        vbox_left.pack_start(label, True, True, 0)
-
-        self.button = Gtk.Button(label = "Click Here")
-        self.button2 = Gtk.Button(label = "Click There")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(hbox)
-        self.add(self.button)
-
-'''
     def on_button_clicked(self, widget):
         print("Hi")
 
